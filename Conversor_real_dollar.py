5af862f5fffd13ed8f5054c78216b5ff3d6e7fc7
Ana Clara Ferreira <98550440+anaclaracrf1@users.noreply.github.com>
2023-03-08 20:11:05 -0300

    add Conversor_real_dollar.py

---
+++ Conversor_real_dollar.py
@@ -0,0 +1,4 @@
+t = int(input('Quantos reais você tem?'))
+cd = int(input('Qual a cotação do dollar em relação ao real hoje?'))
+d = int(t/cd)
+print(f'Você pode comprar aproximadamente {d} dólares com o valor de {t} reais.')
